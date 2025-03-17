db16c1219be0d599591dd1dbb19248a3981520a1
Heipry <heipry@hotmail.com>
2025-03-17 13:19:20 +0100

    Fix ejercicios D

--- js/ejercicio21.js
+++ js/ejercicio21.js
@@ -2,6 +2,6 @@
 let listaDeSuper = [];
 //Con un prompt pido  al usuario para que ingrese al menos 5 productos para llenar la lista de super.
 let listaUsuario = prompt(`Por favor ingrese 5 productos para armar la lista del supermercado, separe cada producto con una coma.`);
-listaDeSuper = listaUsuario.split();
-//muestro por consola los productos
-console.log(`Sus productos son: ${listaDeSuper}`);
+listaDeSuper = listaUsuario.split(",");
+//muestro por consola los productos en el array
+console.log(listaDeSuper);
--- js/ejercicio22.js
+++ js/ejercicio22.js
@@ -4,12 +4,9 @@ let nombres = ["Analia", "Carlos", "Andres", "Agustin", "Mateo","Ana"];
 console.info(nombres);
 //Creo una segunda variable que la llamo nombreMasLargo y la inicializo vacía
 let nombreMasLargo = ""; 
-//Creo una tercer varaible y la llamo NombreMasCorto
-let nombreMasCorto = "";
-//Voy a ir guardando el nombre mas largo a medida que se va ejecutando el for, voy a ir recorriendo uno a uno los nombres y comparando cuál es más largo (que seguarda en nombreMasLargo)
+//Voy a ir guardando el nombre mas largo a medida que se va ejecutando el for, voy a ir recorriendo uno a uno los nombres y comparando cuál es más largo (que se guarda en nombreMasLargo)
 for (let i = 0; i < nombres.length; i++) {
   if (nombres[i].length >= nombreMasLargo.length) {
-    nombreMasCorto = i;
     nombreMasLargo = nombres[i];
   }
 }
